09031a519a42ec25db0f2a88d130f8ba820f84ca
John Manning <john.manning144@gmail.com>
2021-03-12 00:36:29 -0600

    final edits to src

--- stack-overflow-app/src/index.js
+++ stack-overflow-app/src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals'; 
 
-var queryAddress = '';
 var result = [];
 function createQueryAddress(sortType, tag){
-  return 'https://api.stackexchange.com/2.2/search?order=desc&sort='+sortType+ '&tagged='+ tag+ '&site=stackoverflow&filter=!--1nZwQ5_Sz)';
+  var date = Math.round(Date.now() / 1000)
+  var weekAgo = date - 604800;
+  return 'https://api.stackexchange.com/2.2/search?fromdate=' + 
+                                                            weekAgo + '&order=desc&sort='+
+                                                            sortType+ '&tagged='+ 
+                                                            tag+ '&site=stackoverflow&filter=!)rTkraPXxg*xgr03n8Uq';
 }
 
 class InputBox extends React.Component{
   constructor(props){
     super(props);
-    this.state = {input: '', requestData: []}
+    this.state = {input: '', requestData: [], }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,29 +26,39 @@ class InputBox extends React.Component{
   }
 
   handleSubmit(event){
-    queryAddress = createQueryAddress('votes', this.state.input);
-    this.makeRequest();
-    queryAddress = createQueryAddress('creation', this.state.input);
-    this.makeRequest();
-    this.setState({requestData: result})
-    result = [];
-
+    this.makeRequest(createQueryAddress('votes', this.state.input));
     event.preventDefault();
   }
-  makeRequest(){
-      fetch(queryAddress)
+
+  makeRequest(addr){
+      fetch(addr)
           .then(response => response.json())
           .then(data => {
-            for(var i = 0; i< 10; i++ ){
+            for(var i = 0; i < 10; i++ ){
                result[result.length] = data.items[i];
             }
+            this.followUpRequest(createQueryAddress('creation', this.state.input));
            });
+    
+  }
+  followUpRequest(addr){
+    fetch(addr)
+      .then(response => response.json())
+      .then(data => {
+        for(var i = 0; i < 10; i++ ){
+          result[result.length] = data.items[i];
+      }
+      result.sort(function(a, b){return b.creation_date - a.creation_date;})
+      this.setState({requestData: result})
+      result = [];
+     });
+
   }
   render(){
     return(
-      <div>
+      <div className = "input_box">
         <form onSubmit ={this.handleSubmit}>
-          <label>
+          <label className = "input_label">
             Input a Tag to search for:
             <input type = "text" value = {this.state.value} 
               onChange = {this.handleChange}
@@ -60,6 +74,8 @@ class InputBox extends React.Component{
             creation_date = {item.creation_date}
             score = {item.score}
             body = {item.body}
+            comments = {item.comments}
+            answers = {item.answers}
             />   
         ))  
       }
@@ -70,11 +86,14 @@ class InputBox extends React.Component{
 }
 function Post(props){
   return(
-    <ul>
-      <div className = "title"> <h3 dangerouslySetInnerHTML ={{__html: props.title}}></h3> </div>
-        <div className = "creationDate" ><li >Date: {props.creation_date}</li></div>
-        <div className = "votes"><li>Score: {props.score}</li></div>
-        <div className = "content"><ContentToggle body = {props.body}/></div>
+    <ul className = "post">
+        <div className = "title"> <h3 dangerouslySetInnerHTML ={{__html: props.title}}></h3> </div>
+        <PostData creation_date = {props.creation_date} score = {props.score}/>
+        <div className = "content"><ContentToggle 
+                                    body = {props.body} 
+                                    answers = {props.answers} 
+                                    comments = {props.comments}/>
+        </div>
     </ul>
   );
 }
@@ -99,7 +118,7 @@ class ContentToggle extends React.Component{
 
     if(isVisible){
       button = <button onClick ={this.hide}> Hide Content</button>
-      text = <Content body = {this.props.body}/>
+      text = <Content body = {this.props.body} answers = {this.props.answers}/>
     }else{
       button = <button onClick ={this.expand}> Show Content</button>
       text = null;
@@ -110,15 +129,96 @@ class ContentToggle extends React.Component{
   }
 }
 function Content(props){
+  var answerArr = [];
+  var commentArr = [];
+  var commentString = "";
+  var answerString = "";
+  if(props.answers !== undefined){
+    answerArr = props.answers;
+    answerString = "Answers:";
+  }
+
+  if(props.comments!== undefined){
+    commentArr = props.comments;
+    commentString = "Comments";
+  }
   return(
       <div className = "content">
-        <div className = "body" dangerouslySetInnerHTML= {{__html: props.body}}/>;
+        <div className = "body" dangerouslySetInnerHTML= {{__html: props.body}}/>
+          <h4 className = "comment">
+            {commentString}
+          </h4>
+        {
+          commentArr.map((comment) => (
+        <Comment key = {comment.comment_id} 
+            creation_date = {comment.creation_date}
+            score = {comment.score}
+            body = {comment.body}
+            />   
+          )) 
+        }
+        <h4 className = "answer">
+          {answerString}
+        </h4>
+        {
+          answerArr.map((answer) => (
+        <Answer key = {answer.answer_id} 
+            creation_date = {answer.creation_date}
+            score = {answer.score}
+            body = {answer.body}
+            comments = {answer.comments}
+            />   
+          )) 
+        }
       </div>
 
   );
 }
-function Answers(props){
+function PostData(props){
+  return(
+    <div className = "post_data">
+      Date: <DateElement creation_date = {props.creation_date}/> Score: {props.score}
+    </div>
+  );
+}
+function Comment(props){
+  return(
+    <div>
+    <div className = "comment" dangerouslySetInnerHTML = {{__html: props.body}}/>
+    <PostData creation_date = {props.creation_date} score = {props.score}/>
+    <br></br>
+    </div>
+  );
+}
+function Answer(props){
+  var commentArr = [];
+  var commentString = "";
+  if(props.comments!== undefined){
+    commentArr = props.comments;
+    commentString = "Comments";
+  }
+  return(
+    <div>
+      <div className = "answer" dangerouslySetInnerHTML = {{__html: props.body}}/>
+      <PostData creation_date = {props.creation_date} score = {props.score}/>
+      <h5 className = "comment"> {commentString} </h5>
+      {
+          commentArr.map((comment) => (
+        <Comment key = {comment.comment_id} 
+            creation_date = {comment.creation_date}
+            score = {comment.score}
+            body = {comment.body}
+            />   
+          )) 
+        }
+    </div>
+  );
+}
 
+function DateElement(props){
+  return(
+     new Date(props.creation_date * 1000).toLocaleTimeString()
+  );
 }
 function App(){
   return (
